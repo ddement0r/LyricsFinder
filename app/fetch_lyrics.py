03b747c1993c6ab17f496771e987f1c17325b534
Abdelhamid Najmi <abdelhamidnajmi10@gmail.com>
2024-04-03 16:11:18 +0000

    API key replaced

--- app/fetch_lyrics.py
+++ app/fetch_lyrics.py
@@ -1,18 +1,14 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import time
 
 def fetch_lyrics(cleaned_title):
     lyrics = ""
     print(cleaned_title)
-    # Using the example URL directly for demonstration; replace with your formatted URL as needed
-    url = "https://www.lyricsmania.com/instant_crush_lyrics_daft_punk.html"
+    url = f"https://www.lyricsmania.com/{cleaned_title}.html"
     
-    # Added verify=False as per your requirement
     response = requests.get(url, verify=False)
     soup = bs(response.text, 'html.parser')
     
-    # Finding the parent container 'lyrics-body' for the lyrics
     lyrics_container = soup.find('div', class_='lyrics-body')
     print("LYRICS ARE HERE\n \n "+ lyrics_container.text)
     if lyrics_container:
